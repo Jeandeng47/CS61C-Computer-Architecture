002724aff4dea8db713227154bf9c18d4cedcc18
Jeandeng47 <dengziying98@gmail.com>
2024-02-22 16:10:41 -0500

    submitted lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,34 @@
 #include <stddef.h>
 #include "ll_cycle.h"
+#include <stdio.h>
 
-int ll_has_cycle(node *head) {
+int ll_has_cycle(node *head)
+{
     /* your code here */
+
+    // if list empty or only single node, no cycle
+    if (head == NULL || head->next == NULL)
+    {
+        printf("If list is empty or contains only single node, no cycle.\n");
+        return 0;
+    }
+
+    node *tortoise = head;
+    node *hare = head;
+
+    while (hare != NULL && hare->next != NULL)
+    {
+        // tortoise moves 1 step, hare moves 2 steps
+        tortoise = tortoise->next;
+        hare = hare->next->next;
+
+        // if tortoise and hare meets, there is cycle
+        if (tortoise == hare)
+        {
+            return 1;
+        }
+    }
+
+    printf("No cycle detected.\n");
     return 0;
 }
--- lab01/segfault_ex.c
+++ lab01/segfault_ex.c
@@ -1,6 +1,15 @@
-int main() {
+int main()
+{
     int a[20];
-    for (int i = 0; ; i++) {
+
+    // Problem: write ints out of the boundary of the array
+    // for (int i = 0; ; i++) {
+    //     a[i] = i;
+    // }
+
+    // Solution: add terminate condition
+    for (int i = 0; i < 20; i++)
+    {
         a[i] = i;
     }
 }
